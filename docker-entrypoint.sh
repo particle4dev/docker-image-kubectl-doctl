ed0fd041feefcaf893f188dba22cd4a464682578
Nam Hoang <>
2020-08-11 16:57:51 +0700

    saved

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,4 @@
 #!/usr/local/bin/dumb-init /bin/sh
 set -e
 
-# This test if no have "-" in a parameter 1 and command is not available
-# if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-#   set -- node "$@"
-# fi
-
 exec "$@"
